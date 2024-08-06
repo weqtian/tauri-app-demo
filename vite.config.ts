1a6607f3db484bf8d59399d230c788e04d7d9f77
筱羽 <weqtian@outlook.com>
2024-08-06 17:56:59 +0800

    :rocket:Fix build errors

--- vite.config.ts
+++ vite.config.ts
@@ -13,8 +13,6 @@ export default defineConfig({
      port: 8080,
      strictPort: true,
    },
-
-    // to access the Tauri environment variables set by the CLI with information about the current target
   envPrefix: ['VITE_', 'TAURI_PLATFORM', 'TAURI_ARCH', 'TAURI_FAMILY', 'TAURI_PLATFORM_VERSION', 'TAURI_PLATFORM_TYPE', 'TAURI_DEBUG'],
 
   build: {
